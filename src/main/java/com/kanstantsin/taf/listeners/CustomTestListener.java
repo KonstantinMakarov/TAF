fd80528536ca402e73a0e16083dc75d5d46b7e96
Makarau, Kanstantsin <Kanstantsin.Makarau@thomsonreuters.com>
2017-06-26 14:24:01 +0300

    Add Logging

---
+++ src/main/java/com/kanstantsin/taf/listeners/CustomTestListener.java
@@ -0,0 +1,39 @@
+package com.kanstantsin.taf.listeners;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class CustomTestListener implements ITestListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomTestListener.class);
+
+    public void onTestStart(ITestResult result) {
+        LOG.info("Test started running:"  + result.getMethod().getTestClass().getName() + ", at:" + result.getTestContext().getStartDate());
+    }
+
+    public void onTestSuccess(ITestResult result) {
+        LOG.info("Test PASSED successfully");
+    }
+
+    public void onTestFailure(ITestResult result) {
+        LOG.info("Test FAILED successfully");
+    }
+
+    public void onTestSkipped(ITestResult result) {
+        LOG.info("Test SKIPPED successfully");
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    }
+
+    public void onStart(ITestContext context) {
+        LOG.info("Global START at:" + context.getStartDate());
+    }
+
+    public void onFinish(ITestContext context) {
+        LOG.info("Global FINISH at:" + context.getEndDate());
+    }
+}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,11 +1,13 @@
 package tests;
 
 import com.kanstantsin.taf.browser.Browser;
+import com.kanstantsin.taf.listeners.CustomTestListener;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
-
+@Listeners(CustomTestListener.class)
 public abstract class BaseTest {
 
     protected RemoteWebDriver driver;
